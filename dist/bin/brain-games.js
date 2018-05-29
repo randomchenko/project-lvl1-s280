c771b86204cdd10a70e97b698d8c7bfd286a100b
Mona Lisa <mona@lisa.com>
2018-05-29 12:21:26 +0300

    fix and second task

--- dist/bin/brain-games.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-console.log('Welcome to the Brain Games');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7QUFBQUEsUUFBUUMsR0FBUixDQUFZLDRCQUFaIiwiZmlsZSI6ImJyYWluLWdhbWVzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEdhbWVzJyk7XG4iXX0=
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,1 +1,5 @@
+#!/usr/bin/env node
+import greet from '..';
+
 console.log('Welcome to the Brain Games');
+greet();
