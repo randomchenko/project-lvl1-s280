69d93a262201c662361e3aa6246ef1be514750de
Mona Lisa <mona@lisa.com>
2018-06-03 10:45:55 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,8 +4,11 @@ import getRandomNumber from '../utils';
 
 const rule = 'Is this number prime?';
 const isPrime = (question) => {
-  if (question < 4 || question % 2 === 0) {
+  if (question === 2) {
     return true;
+  }
+  if (question <= 1 || question % 2 === 0) {
+    return false;
   }
   const maxDivisor = Math.floor(Math.sqrt(question));
   for (let j = 3; j <= maxDivisor; j += 1) {
